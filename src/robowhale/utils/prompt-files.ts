9d2bbd74f608554c531b3200348dd1e64303d33c
Vladislav Forsh <vlad@robowhale.com>
2022-01-10 20:45:20 +0300

    add util method promptFiles

---
+++ src/robowhale/utils/prompt-files.ts
@@ -0,0 +1,37 @@
+import { createHtmlElement } from "./create-html-element"
+
+export type FilePromptAccept =
+	| string
+	| "application/json"
+	| "image/*"
+	| "audio/*"
+	| "video/*"
+
+export interface FilePromptOptions {
+	fileTypes: FilePromptAccept | FilePromptAccept[]
+	allowMultiple?: boolean
+}
+
+export function promptFiles(options: FilePromptOptions): Promise<File[]> {
+	return new Promise<File[]>((resolve, reject) => {
+		let uploadInput = createHtmlElement(`<input type="file">`) as HTMLInputElement
+		uploadInput.style.display = "none"
+		uploadInput.addEventListener("change", () => {
+			let filesArr = Array.from(uploadInput.files)
+			uploadInput.remove()
+			resolve(filesArr)
+		}, { once: true })
+		
+		let accept = Array.isArray(options.fileTypes)
+			? options.fileTypes.join(", ")
+			: options.fileTypes
+		
+		uploadInput.setAttribute("accept", accept)
+		
+		if (options.allowMultiple) {
+			uploadInput.setAttribute("multiple", "")
+		}
+		
+		uploadInput.click()
+	})
+}
