6d9b55453a5ffb3b64f501f7fdb315b280400c24
Vladislav Forsh <vlad@robowhale.com>
2022-07-23 13:02:41 +0300

    fix typo

--- scripts/build/dev.js
+++ scripts/build/dev.js
@@ -2,20 +2,24 @@ const esbuild = require("esbuild")
 const { external } = require("./helpers")
 const timestamp = require("time-stamp")
 const chalk = require("chalk")
+const argv = require('minimist')(process.argv.slice(2))
+
+const watch = argv['watch']
+const outfile = "dev/js/game.js"
 
 function getTimestamp() {
 	return chalk.green(timestamp("[YYYY/MM/DD HH:mm:ss]"))
 }
 
 esbuild.build({
-	entryPoints: ["../../src/startup/Startup.ts"],
-	outfile: "../../dev/js/game.js",
+	entryPoints: ["src/startup/Startup.ts"],
+	outfile: outfile,
 	bundle: true,
-	target: "es6",
+	target: "es2020",
 	platform: "browser",
 	external,
 	sourcemap: "inline",
-	watch: {
+	watch: watch && {
 		onRebuild(error, result) {
 			let time = getTimestamp()
 
@@ -27,5 +31,9 @@ esbuild.build({
 		},
 	},
 }).then((result) => {
-	console.log(getTimestamp(), "Initial build is ready")
+	if (watch) {
+		console.log(getTimestamp(), "Initial build is ready")
+	} else {
+		console.log(`Build is ready at ${outfile}`)
+	}
 })
